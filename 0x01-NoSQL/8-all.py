ff9a9ed8208a5df70716a9306107def11b51343b
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-17 22:44:34 +0100

    task 8, use pymongo librairie to list all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    
+    Args:
+        mongo_collection : The pymongo collection object.
+    Return an empty list if no document in the collection
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
