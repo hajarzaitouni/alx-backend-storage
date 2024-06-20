93b5f3535a28cae4356e83ee9a065773c194ca02
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-20 02:20:45 +0100

    update exercise by adding get method to cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Define Module Writing strings to Redis
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -24,3 +24,25 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves the data stored in Redis for the given key
+        and apply an optional conversion function to the data.
+        """
+        stored_data = self._redis.get(key)
+        if stored_data is None:
+            return None
+        if fn:
+            return fn(stored_data)
+        return stored_data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve the stored data for the given key and convert to string"""
+        stored_data = self._redis.get(key)
+        return stored_data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Retrieve the data stored in Redis and convert it to an integer """
+        return self.get(key, lambda s_data: int(s_data))
